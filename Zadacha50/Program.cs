cc06941c114e9f1d9c1372eb0be249809e5abfd7
Viktor Chen <chenviktor87@gmail.com>
2023-05-05 16:36:37 +0500

    add Zadacha50

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,48 @@
+﻿int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, colums];
+int idRows = ReadInt("Введите строку: ");
+int idColums = ReadInt("Введите столбец: ");
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if ((idRows < rows)&(idColums < colums))
+{
+    Console.WriteLine(numbers[idRows, idColums]);
+}
+else
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-9, 9);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
